52b747a3e7918c95482ef7fc038573ee1a7b1df3
Lisa Harrylock <lisah@alleninstitute.org>
2018-10-22 10:26:34 -0700

    add barcode and modal to metadata entry

--- src/renderer/containers/MetadataEntry/index.tsx
+++ src/renderer/containers/MetadataEntry/index.tsx
@@ -1,114 +1,143 @@
-import { AicsCellPopulationBatches, AicsCellPopulationGeneral, Stage } from "aics-react-labkey-internal";
+import { LabKeyOptionSelector } from "aics-react-labkey";
 import "aics-react-labkey-internal/dist/styles.css";
+import { Button, Modal } from "antd";
 import * as classNames from "classnames";
+import { debounce } from "lodash";
 import * as React from "react";
 import { connect } from "react-redux";
 
 import {
     State,
 } from "../../state";
+import { getSelectedFiles } from "../../state/selection/selectors";
 
 const styles = require("./style.css");
+const BARCODES = [
+    "550000012",
+    "3500002386",
+    "3500002385",
+    "3500002370",
+];
 
 interface Props {
     className?: string;
+    files?: string[];
 }
 
 interface MetadataEntryState {
-    cellLine?: string;
-    clone?: string;
-    diffCellProtocol?: string;
-    diffCellType?: string;
-    // diffReadOnly: boolean;
-    diffTime?: string;
-    donorPlasmid: string;
-    donorPlasmidBatch?: string;
-    gene: string;
-    includeClone?: boolean;
-    isDifferentiated?: boolean;
-    passage?: string;
-    stage?: Stage;
-    tagLocation: string;
+    barcode?: string;
+    confirmCreatePlateModalLoading: boolean;
+    error?: string;
+    showCreatePlateModal: boolean;
 }
 
-const STAGE_OPTIONS = [
-    { Name: "Pre-FACS Enrichment", StageId: 1 },
-    { Name: "Post-FACS Enrichment", StageId: 2 },
-    { Name: "Etcetera", StageId: 3 },
-];
-
-const CELL_LINE_OPTIONS = [
-    { Name: "AICS-0", CellLineId: 1 },
-];
-
-const CRRNA_BATCH_OPTIONS = [
-    { Name: "CrRnaBatch1", CrRnaId: 1 },
-];
-
-const DONOR_PLASMID_BATCH_OPTIONS = [
-    { Name: "DonorPlasmidBatch1", DonorPlasmidBatchId: 1 },
-];
-
-const CAS_9_BATCH_OPTIONS = [
-    { Name: "Cas9Batch1", Cas9BatchId: 1 },
-];
+interface BarcodeOption {
+    barcode: string;
+}
 
 class MetadataEntry extends React.Component<Props, MetadataEntryState> {
+    private static getBarcodesAsync(input): Promise<{options: BarcodeOption[]} | null> {
+        if (!input) {
+            return Promise.resolve(null);
+        }
+
+        return new Promise((resolve) => setTimeout(resolve, 1000, {
+            options: BARCODES.map((b: string) => ({barcode: b})),
+        }));
+        // It's a good idea to prime the cache this way when debouncing
+        // this.asyncCache = { '': [] };
+    }
+
     constructor(props: {}) {
         super(props);
         this.state = {
-            // batchesReadOnly: false,
-            // batchesRequired: false,
-            // cellLine: null,
-            clone: "",
-            // diffCellProtocol: null,
-            // diffCellType: null,
-          //  diffReadOnly: false,
-            diffTime: "",
-            donorPlasmid: "plasmid",
-            // donorPlasmidBatch: null,
-            gene: "PXN",
-            // generalReadOnly: false,
-            includeClone: false,
-            isDifferentiated: false,
-            passage: "",
-            // stage: null,
-            tagLocation: "N-terminus",
+            confirmCreatePlateModalLoading: false,
+            showCreatePlateModal: false,
         };
-
-        this.handleGeneralDataChange = this.handleGeneralDataChange.bind(this);
+        this.setBarcode = this.setBarcode.bind(this);
+        this.openCreatePlateModal = this.openCreatePlateModal.bind(this);
+        this.handleCreatePlateModalOk = this.handleCreatePlateModalOk.bind(this);
+        this.handleCreatePlateModalCancel = this.handleCreatePlateModalCancel.bind(this);
+        MetadataEntry.getBarcodesAsync = debounce(MetadataEntry.getBarcodesAsync, 500);
     }
 
-    public handleGeneralDataChange() {
-        return true;
+    public setBarcode(option: BarcodeOption): void {
+        this.setState(option);
+    }
+
+    public openCreatePlateModal(): void {
+        this.setState({showCreatePlateModal: true});
+    }
+
+    public handleCreatePlateModalOk(): void {
+        this.setState({
+            confirmCreatePlateModalLoading: true,
+        });
+        setTimeout(() => {
+            this.setState({
+                confirmCreatePlateModalLoading: false,
+                showCreatePlateModal: false,
+            });
+        }, 2000);
+    }
+
+    public handleCreatePlateModalCancel(): void {
+        this.setState({showCreatePlateModal: false});
     }
 
     public render() {
-        const {className} = this.props;
-        const {
-            donorPlasmid,
-            gene,
-            tagLocation,
-        } = this.state;
+        const {className, files} = this.props;
+        const {confirmCreatePlateModalLoading, showCreatePlateModal} = this.state;
+        const noFiles = !files || files.length === 0;
         return (
             <div
                 className={classNames(className, styles.container)}
             >
-                <AicsCellPopulationGeneral
-                    cellLineOptions={CELL_LINE_OPTIONS}
-                    stageOptions={STAGE_OPTIONS}
-                    isDifferentiated={false}
-                    handleDataChange={this.handleGeneralDataChange}
-                />
-                <AicsCellPopulationBatches
-                    donorPlasmid={donorPlasmid}
-                    gene={gene}
-                    tagLocation={tagLocation}
-                    crRnaBatchOptions={CRRNA_BATCH_OPTIONS}
-                    donorPlasmidBatchOptions={DONOR_PLASMID_BATCH_OPTIONS}
-                    cas9BatchOptions={CAS_9_BATCH_OPTIONS}
-                    readOnly={false}
-                />
+                <h1>Metadata</h1>
+                {noFiles &&
+                <div className={styles.noDocumentsText}>
+                    <div>No files selected.</div>
+                    <div>Select one or more files to get started.</div>
+                </div>}
+                {!noFiles && files != null &&
+                (
+                    <div>
+                        <h2>Selected Files:</h2>
+                        {files.map((file: string) => <div key={file}>{file}</div>)}
+                    </div>
+                )}
+                <div>
+                    Plate Barcode:
+                    <LabKeyOptionSelector
+                        required={true}
+                        async={true}
+                        id="single-selector-async"
+                        label="Plate Barcode"
+                        optionIdKey="barcode"
+                        optionNameKey="barcode"
+                        selected={this.state.barcode}
+                        onOptionSelection={this.setBarcode}
+                        disabled={false}
+                        error={this.state.error}
+                        clearable={true}
+                        placeholder="Enter barcode"
+                        helpText="Enter barcode if plate exists or click 'Create Plate'"
+                        loadOptions={MetadataEntry.getBarcodesAsync}
+                        autoload={false}
+                    />
+                    <Button onClick={this.openCreatePlateModal}>
+                        Create Plate
+                    </Button>
+                    <Modal
+                        title="Title"
+                        visible={showCreatePlateModal}
+                        onOk={this.handleCreatePlateModalOk}
+                        confirmLoading={confirmCreatePlateModalLoading}
+                        onCancel={this.handleCreatePlateModalCancel}
+                    >
+                        <p>Labkey</p>
+                    </Modal>
+                </div>
             </div>
         );
     }
@@ -117,6 +146,7 @@ class MetadataEntry extends React.Component<Props, MetadataEntryState> {
 function mapStateToProps(state: State, props: Props): Props {
     return {
         className: props.className,
+        files: getSelectedFiles(state),
     };
 }
 
