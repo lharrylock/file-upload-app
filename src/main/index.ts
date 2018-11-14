61cf216d0f15e87adf0ff42e288fe8472a082de2
Lisa Harrylock <lisah@alleninstitute.org>
2018-11-14 11:17:19 -0800

    disable web security

--- src/main/index.ts
+++ src/main/index.ts
@@ -10,7 +10,13 @@ let win: BrowserWindow | undefined;
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600});
+    win = new BrowserWindow({
+        height: 600,
+        webPreferences: {
+            webSecurity: false,
+        },
+        width: 800,
+    });
 
     // and load the index.html of the app.
     win.loadURL(url.format({
