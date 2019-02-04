ef3a97b69b708da828ed9d058d2af43398dfe87d
lisajoy <lisaharrylock@gmail.com>
2019-02-04 15:28:26 -0800

    use env variables for mms base url

--- src/renderer/constants/index.ts
+++ src/renderer/constants/index.ts
@@ -2,16 +2,15 @@ import { isEmpty } from "lodash";
 
 export const APP_ID = "file-upload-app";
 
-// Metadata Management Service
-export const MMS_VERSION = "1.0";
-export const MMS_BASE_URL = `/metadata-management-service/${MMS_VERSION}`;
-
-// Labkey
 const {
     LABKEY_PROTOCOL = "http",
     LABKEY_HOST = "localhost",
     LABKEY_PORT = "8080",
+    MMS_PROTOCOL = "http",
+    MMS_HOST = "localhost",
+    MMS_PORT = "8080",
 } = process.env;
+
 export const LABKEY_URL = `${LABKEY_PROTOCOL}://${LABKEY_HOST}:${LABKEY_PORT}/labkey`;
 export const LABKEY_SELECT_ROWS_URL = (schema: string, table: string, additionalQueries: string[] = []) => {
     const base = `${LABKEY_URL}/AICS/query-selectRows.api?schemaName=${schema}&query.queryName=${table}`;
@@ -21,3 +20,7 @@ export const LABKEY_SELECT_ROWS_URL = (schema: string, table: string, additional
 
     return base;
 };
+
+// Metadata Management Service
+// todo: FMS-397 use env variables for protocol, host, and port
+export const MMS_BASE_URL = `${MMS_PROTOCOL}://${MMS_HOST}:${MMS_PORT}/metadata-management-service`;
