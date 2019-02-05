21f59e901d936f6895fedbff0790dea67664e4c1
lisajoy <lisaharrylock@gmail.com>
2019-02-04 16:01:51 -0800

    Rename action and request metadata on app mount

--- src/renderer/state/metadata/types.ts
+++ src/renderer/state/metadata/types.ts
@@ -1,12 +1,27 @@
 export interface MetadataStateBranch {
     [key: string]: any;
+    units?: Unit[];
 }
 
-export interface ReceiveAction {
+export interface ReceiveMetadataAction {
     payload: MetadataStateBranch;
     type: string;
 }
 
-export interface RequestAction {
+export interface RequestMetadataAction {
     type: string;
+}
+
+export interface Unit {
+    description: string;
+    name: string;
+    type: string;
+    unitsId: number;
+}
+
+export interface LabkeyUnit {
+    Type: string;
+    Description: string;
+    UnitsId: number;
+    Name: string;
 }
