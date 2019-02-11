041831d3a2d8a26b6f31205ff93e39d0da39ec53
lisajoy <lisaharrylock@gmail.com>
2019-02-11 11:26:08 -0800

    remove grid types

--- src/renderer/@types/aics-react-labkey/labkey-option-selector.d.ts
+++ src/renderer/@types/aics-react-labkey/labkey-option-selector.d.ts
@@ -42,29 +42,4 @@ declare module "aics-react-labkey" {
         constructor(props: LabkeyOptionSelectorProps);
         public render(): JSX.Element | null;
     }
-
-    export interface AicsGridCell {
-        row: number;
-        col: number;
-    }
-
-    export interface AicsGridProps {
-        selectedCells: AicsGridCell[];
-        selectMode: "multi" | "single" | "contiguous" | "non-contiguous" | "disabled";
-        cellHeight?: string;
-        cellWidth?: string;
-        fontSize?: string;
-        onCellClick?: (event: React.MouseEvent<HTMLDivElement>, row: number, col: number, data: any) => void;
-        onSelectedCellsChanged?: (cells: AicsGridCell[]) => void;
-        // cell keys look like `${rowIndex},${colIndex}`
-        displayBackground: (cellData: any) => string;
-        displayText: (cellData: any) => string | JSX.Element;
-        cells: any[][];
-    }
-
-    // tslint:disable-next-line
-    export class AicsGrid extends React.Component<AicsGridProps, {}> {
-        constructor(props: AicsGridProps);
-        public render(): JSX.Element | null;
-    }
 }
