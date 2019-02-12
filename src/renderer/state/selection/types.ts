502815b322f17ad5dcb90f60d910d3758d1cd649
lisajoy <lisaharrylock@gmail.com>
2019-02-12 10:45:35 -0800

    add plateId to SelectionStateBranch

--- src/renderer/state/selection/types.ts
+++ src/renderer/state/selection/types.ts
@@ -17,6 +17,7 @@ export interface DeselectFileAction {
 export interface SelectionStateBranch {
     [key: string]: any;
     barcode?: string;
+    plateId?: number;
     page: AppPage;
     stagedFiles: UploadFile[];
 }
