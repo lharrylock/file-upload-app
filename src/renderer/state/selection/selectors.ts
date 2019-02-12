7931254aed36f08daa9951c34dad2bb1858469dd
lisajoy <lisaharrylock@gmail.com>
2019-02-12 11:25:59 -0800

    map over wells to add unit display values

--- src/renderer/state/selection/selectors.ts
+++ src/renderer/state/selection/selectors.ts
@@ -1,4 +1,10 @@
+import { createSelector } from "reselect";
+import { getUnits } from "../metadata/selectors";
+
+import { Unit } from "../metadata/types";
 import { State } from "../types";
+
+import { Solution, SolutionLot, ViabilityResult, Well } from "./types";
 
 // BASIC SELECTORS
 export const getSelectedBarcode = (state: State) => state.selection.barcode;
@@ -6,3 +12,46 @@ export const getSelections = (state: State) => state.selection;
 export const getSelectedFiles = (state: State) => state.selection.files;
 export const getAppPage = (state: State) => state.selection.page;
 export const getStagedFiles = (state: State) => state.selection.stagedFiles;
+export const getWells = (state: State) => state.selection.wells;
+
+// COMPOSED SELECTORS
+const NO_UNIT = "";
+export const getWellsWithUnits = createSelector([
+    getWells,
+    getUnits,
+], (wells?: Well[][], units?: Unit[]): Well[][] | undefined => {
+    if (!wells || !units) {
+        return wells;
+    }
+
+    return wells.map((wellRow: Well[]) => wellRow.map((well) => {
+        const solutions: Solution[] = well.solutions.map((s: Solution) => {
+            const volumeUnit: Unit | undefined = units.find((u) => u.unitsId === s.volumeUnitId);
+            const concentrationUnit: Unit | undefined = units
+                .find((u) => u.unitsId === s.solutionLot.concentrationUnitsId);
+            const solutionLot: SolutionLot = {
+                ...s.solutionLot,
+                concentrationUnitsDisplay: concentrationUnit ? concentrationUnit.name : NO_UNIT,
+            };
+            return {
+                ...s,
+                volumeUnitDisplay:  volumeUnit ? volumeUnit.name : NO_UNIT,
+                ...solutionLot,
+            };
+        });
+        const viabilityResults: ViabilityResult[] = well.viabilityResults.map((v: ViabilityResult) => {
+            const suspensionVolumUnit: Unit | undefined = units.find((u) => u.unitsId === v.suspensionVolumeUnitId);
+            const viableCellCountUnit: Unit | undefined = units.find((u) => u.unitsId === v.viableCellCountUnitId);
+            return {
+                ...v,
+                suspensionVolumeUnitDisplay: suspensionVolumUnit ? suspensionVolumUnit.name : NO_UNIT,
+                viableCellCountUnitDisplay: viableCellCountUnit ? viableCellCountUnit.name : NO_UNIT,
+            };
+        });
+        return {
+            ...well,
+            solutions,
+            viabilityResults,
+        };
+    }));
+});
