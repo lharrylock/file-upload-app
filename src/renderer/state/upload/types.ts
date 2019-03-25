77f15521046fde77edbeb6a79ff592af11e09738
Lisa Harrylock <lisah@alleninstitute.org>
2019-03-25 14:18:07 -0700

    remove uploadaction

--- src/renderer/state/upload/types.ts
+++ src/renderer/state/upload/types.ts
@@ -38,10 +38,6 @@ export interface ClearUploadHistoryAction {
     type: string;
 }
 
-export interface UploadAction {
-    type: string;
-}
-
 // Represents information needed to display an Antd Tag next to a file on the FolderTree.
 // There will be a tag for each piece of metadata associated with a file.
 export interface FileTag {
