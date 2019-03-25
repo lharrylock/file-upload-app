b339c32da1254775c543c69112433b541c7dee65
Lisa Harrylock <lisah@alleninstitute.org>
2019-03-25 13:29:36 -0700

    add more info to upload

--- src/renderer/state/upload/logics.ts
+++ src/renderer/state/upload/logics.ts
@@ -1,11 +1,19 @@
 import { createLogic } from "redux-logic";
+import { getWellLabel } from "../../util";
 import { deselectFiles } from "../selection/actions";
+import { getSelectedBarcode, getWell } from "../selection/selectors";
 import { ReduxLogicNextCb, ReduxLogicTransformDependencies } from "../types";
 import { batchActions } from "../util";
 import { ASSOCIATE_FILES_AND_WELL } from "./constants";
 
 const associateFileAndWellLogic = createLogic({
-    transform: ({action}: ReduxLogicTransformDependencies, next: ReduxLogicNextCb) => {
+    transform: ({action, getState}: ReduxLogicTransformDependencies, next: ReduxLogicNextCb) => {
+        const state = getState();
+        action.payload = {
+            ...action.payload,
+            barcode: getSelectedBarcode(state),
+            wellLabel: getWellLabel(getWell(state)),
+        };
         next(batchActions([
             action,
             deselectFiles(),
