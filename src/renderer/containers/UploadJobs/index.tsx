1f499c8796b5f39acf297fbc8ce14362ff6c3dd8
Lisa Harrylock <lisah@alleninstitute.org>
2019-03-28 14:18:42 -0700

    Add all data on all pages selector

--- src/renderer/containers/UploadJobs/index.tsx
+++ src/renderer/containers/UploadJobs/index.tsx
@@ -62,8 +62,18 @@ class UploadJobs extends React.Component<Props, UploadJobsState> {
 
     private get rowSelection() {
         return {
+            hideDefaultSelections: true,
             onChange: this.onSelectChange,
             selectedRowKeys: this.state.selectedFiles,
+            selections: [
+                {
+                    key: "all-data",
+                    onSelect: () => this.setState({
+                        selectedFiles: this.props.uploads.map((u) => u.file),
+                    }),
+                    text: "Select all pages",
+                },
+            ],
         };
     }
 
